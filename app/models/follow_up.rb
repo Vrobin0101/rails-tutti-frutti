2dd52f6b6a9b623b5c912a7017ec9f56f74da781
Robin Varetz <vrobin@student.42.fr>
2022-05-30 15:20:05 +0200

    add pundit

--- app/models/follow_up.rb
+++ app/models/follow_up.rb
@@ -2,5 +2,5 @@ class FollowUp < ApplicationRecord
   belongs_to :user
   belongs_to :product
   validates :month_number, :carbon_calcul, :local, :bio, presence: true
-  validates :month_number, acceptance: { accept: (1..12) }
+  vpalidates :month_number, acceptance: { accept: (1..12) }
 end
