5bdeb28e5d1ac8571e54b89471672864b27e64bf
Thibault Chassine <thibault.chassine@gmail.com>
2022-05-30 14:48:25 +0200

    added validations

--- app/models/follow_up.rb
+++ app/models/follow_up.rb
@@ -1,4 +1,6 @@
 class FollowUp < ApplicationRecord
   belongs_to :user
   belongs_to :product
+  validates :month_number, :carbon_calcul, :local, :bio, presence: true
+  validates :month_number, acceptance: { accept: (1..12) }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, :category, :sub_category, :description, :start_month, :end_month, :localable, presence: true
+  validates :start_month, :end_month, acceptance: { accept: (1..12) }
+  has_many :follow_ups
 end
