824f6e4fde7c18da6dc2ec33a41b0fdeb637cbc2
Robin Varetz <vrobin@student.42.fr>
2022-05-30 15:52:34 +0200

    add user to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Destroy products..."
+Product.destroy_all
+puts "Destroy follow ups..."
+FollowUp.destroy_all
+puts "Destroy users.."
+User.destroy_all
+
+10.times do
+  user = User.new(username: Faker::Internet.username,first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "azerty", city: "Paris")
+  user.save!
+end
