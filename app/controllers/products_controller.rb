01b6c746e71b05f1b56d5fc40dcf8c17eae86453
Thibault Chassine <thibault.chassine@gmail.com>
2022-05-30 14:59:49 +0200

    added index and show to product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,13 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: :show
+
+  def index
+    @products = Product.all
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 end
