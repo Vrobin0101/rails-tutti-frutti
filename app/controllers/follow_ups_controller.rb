5e8de70a1c51b88d9862b324fb8e3dda189bbf7f
Thibault Chassine <thibault.chassine@gmail.com>
2022-05-30 15:05:57 +0200

    added private method to follow up controller

--- app/controllers/follow_ups_controller.rb
+++ app/controllers/follow_ups_controller.rb
@@ -1,2 +1,16 @@
 class FollowUpsController < ApplicationController
+
+  private
+
+  def set_follow_up
+    @follow_up = FollowUp.find(params[:id])
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+  def params_follow_up
+    params.require(:follow_up).permit(:month_number, :carbon_calcul, :local, :bio)
+  end
 end
