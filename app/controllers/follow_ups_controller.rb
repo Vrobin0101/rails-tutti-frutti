748df5b5fb5a4e8ba6342a40a8072c350f4678b4
Thibault Chassine <thibault.chassine@gmail.com>
2022-05-31 13:06:59 +0200

    added calcul note and grade

--- app/controllers/follow_ups_controller.rb
+++ app/controllers/follow_ups_controller.rb
@@ -50,9 +50,9 @@ class FollowUpsController < ApplicationController
 
   def calcul_follow_up
     sum = 0
-    if (@product.start_month..@product.end_month).include? @follow_up.month_number
-    elsif @follow_up.local == true
-    elsif @follow_up.bio == true
-    end
+    sum += 75 if (@product.start_month..@product.end_month).include? @follow_up.month_number
+    sum += 15 if @follow_up.local
+    sum += 10 if @follow_up.bio
+    return sum
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,10 +5,30 @@ class PagesController < ApplicationController
   end
 
   def profile
+    tutti_score_global
+    tutti_score_current_month
   end
 
   private
 
-  def balance
+  def note(score)
+    case score
+    when 0...20 then 'E'
+    when 20...40 then 'D'
+    when 40...60 then 'C'
+    when 60...80 then 'B'
+    when 80..100 then 'A'
+    else -1
+    end
+  end
+
+  def tutti_score_global
+    @global_score = current_user.total_average
+    @global_note = note(@global_score)
+  end
+
+  def tutti_score_current_month
+    @current_score = current_user.total_month_average
+    @current_note = note(@current_score)
   end
 end
