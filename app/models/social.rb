79dcdf83873d1d9b876e118976ff61d131878d6a
Robin Varetz <vrobin@student.42.fr>
2022-06-03 14:55:37 +0200

    add validation and minor modification on profile view

--- app/models/social.rb
+++ app/models/social.rb
@@ -1,4 +1,8 @@
 class Social < ApplicationRecord
   belongs_to :asker, class_name: "User"
   belongs_to :receiver, class_name: "User"
+  validates :asker, :receiver, presence: true
+  validates :asker, comparison: { other_than: :receiver }
+  validates :asker, uniqueness: { scope: :receiver }
+  validates :receiver, uniqueness: { scope: :asker }
 end
