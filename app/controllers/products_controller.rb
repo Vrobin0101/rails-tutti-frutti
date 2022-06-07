0b27f13a1c116fa3f4cf0a01135b00915fdc0090
Robin Varetz <vrobin@student.42.fr>
2022-06-07 16:58:51 +0200

    fix index for scrapping

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -68,7 +68,7 @@ class ProductsController < ApplicationController
     html_doc = doc.search(".MRTN__sc-1gofnyi-2")
     @recipes = []
     3.times do |index|
-      @recipes << { name: html_doc[index].search(".MRTN__sc-30rwkm-0").text, note: html_doc[index].search(".SHRD__sc-10plygc-0").text, link:  "https://www.marmiton.org#{html_doc[0].attribute('href').value}"}
+      @recipes << { name: html_doc[index].search(".MRTN__sc-30rwkm-0").text, note: html_doc[index].search(".SHRD__sc-10plygc-0").text, link:  "https://www.marmiton.org#{html_doc[index].attribute('href').value}"}
     end
   end
 end
